44049d79794430aa1e9bb46bcb19a970e8a465bf
Jacob Gorton <jagorton@iastate.edu>
2021-09-22 17:05:14 -0500

    added comments for functions

--- dca1d.c
+++ dca1d.c
@@ -1,18 +1,38 @@
+/**
+ * Author jacob gorton
+ * 
+ * Program to model basic behavior of DCA 
+ */
+
 #include <stdio.h>
 #include "./dca1d.h"
 
+
+/**
+ * has parameters for the number of cells and the initial address
+ * initilaizes all the elements to 0
+ */
 void init1DCA(int numCells, unsigned char* address){
     for(int i = 0; i < numCells; i++){
         address[i]=0;
     }
 }
 
+/**
+ * has parameters for the starting address, the index to be changed, and the new state
+ * function checks of the new state is below 0 and if it is not then sets the 
+ * element of the array at the specified index to the new state
+ */
 void set1DCACell(unsigned char* address, int index, unsigned char state){
     if(index >= 0){
         address[index]=state;
     }
 }
 
+/**
+ * has parameters for the starting address and the number of cells to print
+ * uses a for-loop to print the first numCells cells from the array starting at the given index
+ */
 void display1DCA(unsigned char* address, int numCells){
     for(int i = 0; i < numCells; i++){
         printf("%u ", address[i]);
