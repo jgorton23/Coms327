b7527a723a18c19cacdbfc253e437eaef57504b7
Jacob Gorton <jagorton@iastate.edu>
2021-10-08 18:07:08 -0500

    converted display1DCA method to use struct

--- ca.c
+++ ca.c
@@ -2,7 +2,12 @@
 #include "ca.h"
 
 void display1DCA(CAPTR pointer){
-
+    int x = pointer->numElements;
+    unsigned char* address = pointer->arrayAddress;
+    for(int i = 0; i < x; i++){
+        printf("%u ", address[i]);
+    }
+    printf("\n");
 }
 
 int set1DCACell(CAPTR pointer, unsigned int index, unsigned char value){
@@ -10,5 +15,9 @@ int set1DCACell(CAPTR pointer, unsigned int index, unsigned char value){
 }
 
 void init1DCA(CAPTR pointer, int numElements){
+
+}
+
+CAPTR create1DCA(int size, unsigned char value){
 
 }
