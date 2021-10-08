ed7a0fbe3760e6b4376fc351bd403d1ebc7b458b
Jacob Gorton <jagorton@iastate.edu>
2021-10-08 18:04:44 -0500

    added ca files

---
+++ ca.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "ca.h"
+
+void display1DCA(CAPTR pointer){
+
+}
+
+int set1DCACell(CAPTR pointer, unsigned int index, unsigned char value){
+
+}
+
+void init1DCA(CAPTR pointer, int numElements){
+
+}
---
+++ ca.h
@@ -0,0 +1,26 @@
+#ifndef CA
+#define CA
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+typedef struct ca_t{
+    unsigned char* arrayAddress;
+    int numElements;
+}ca_data;
+
+typedef struct ca_t* CAPTR;
+
+void display1DCA(CAPTR pointer);
+
+int set1DCACell(CAPTR pointer, unsigned int index, unsigned char value);
+
+void init1DCA(CAPTR pointer, int numElements);
+
+CAPTR create1DCA(int size, unsigned char value);
+
+
+#endif
